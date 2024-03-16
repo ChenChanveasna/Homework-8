cd26d1e3da519120156f0e785d4e17014eacbf98
youlengg <2024001ghov@aupp.edu.kh>
2024-03-16 10:31:21 +0700

    Exercise 3

--- exercise3.py
+++ exercise3.py
@@ -1,2 +1,9 @@
 def remove_all_after(numbers, n):
-    ...
+    new_list = []
+    for x in numbers:
+        if x != n:
+            new_list.append(x)
+        elif x == n:
+            new_list.append(x)
+            break
+    return new_list
